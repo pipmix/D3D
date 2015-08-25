4b0cf3a8620e697dc0a0f4e166774d1a2f8ee390
pescobedo <onloadx@yahoo.com>
2015-08-25 18:13:26 -0500

    -Updated obj exporter

--- GameMKII/loadOBJ.cpp
+++ GameMKII/loadOBJ.cpp
@@ -4,8 +4,64 @@
 
 
 
-loadOBJ::loadOBJ(string filename)
+loadOBJ::loadOBJ(wstring filename)
 {
+
+	wifstream fileIn(filename.c_str());
+	if (fileIn) {
+
+		while (fileIn) {
+			curChar = fileIn.get();
+
+
+			switch (curChar) {
+				case '#':
+					curChar = fileIn.get();
+					while(curChar != '\n') curChar = fileIn.get();
+					break;
+				case 'v':
+					curChar = fileIn.get();
+					if (curChar == ' ') {
+						float x, y, z;
+						fileIn >> x >> y >> z;
+						if (rh)	pos.push_back(XMFLOAT3(x, y, z * -1.0f));	
+						else pos.push_back(XMFLOAT3(x, y, z));
+					}
+					if (curChar == 't') {
+						float u, v;
+						fileIn >> u >> v;
+						if (rh)	uv.push_back(XMFLOAT2(u, 1.0f - v));
+						else uv.push_back(XMFLOAT2(u,v));
+						hasUv = true;
+					}
+					if (curChar == 'n') {
+						float x, y, z;
+						fileIn >> x >> y >> z;
+						if (rh)normal.push_back(XMFLOAT3(x,y,z * -1.0f));
+						else normal.push_back(XMFLOAT3(x, y, z));
+						hasNormal = true;
+					}
+				break;
+				case 'g':
+					curChar = fileIn.get();
+					if (curChar == ' ') {
+						posIndex.push_back(vIndex);
+						groupCount++;
+					}
+				break;
+
+
+			}
+
+
+
+		}
+
+
+
+
+	}
+
 }
 
 loadOBJ::~loadOBJ()
--- GameMKII/loadOBJ.h
+++ GameMKII/loadOBJ.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <DirectXMath.h>
+#include <fstream>
 using namespace std;
 using namespace DirectX;
 
@@ -9,10 +10,19 @@ class loadOBJ{
 
 
 public:
-	loadOBJ(string filename);
+	loadOBJ(wstring filename);
 	~loadOBJ();
 
 private:
+
+	vector<unsigned long> indices;
+	vector<XMFLOAT3> pos;
+	vector<XMFLOAT3> normal;
+	vector<XMFLOAT2> uv;
+
+	vector<int> posIndex;
+	vector<int> normalIndex;
+	vector<int> uvIndex;
 
 
 	void addVertex(float, float, float);
@@ -20,6 +30,19 @@ private:
 	vector<XMFLOAT3> vertices;
 
 
+
+	ID3D11Buffer* vertBuff;
+	ID3D11Buffer* indexBuff;
+
+	wchar_t curChar;
+
+	bool rh = false;
+	bool hasUv = false;
+	bool hasNormal = false;
+	int vIndex = 0;
+
+	int groups = 0;
+	vector<int> groupIndexStart;
 
 
 };
